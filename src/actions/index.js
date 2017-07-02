9dd4a5999d1a3cc8f088be02ff95de63ea2c252c
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-02 15:41:24 +0530

    adding a action creator for fetch the POST to backend as soon as the onSubmit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=ARSENAL1234';
@@ -11,4 +12,14 @@ const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+
+export function createPost(values){
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return{
+    type: CREATE_POST,
+    payload: request
+  }
 }
