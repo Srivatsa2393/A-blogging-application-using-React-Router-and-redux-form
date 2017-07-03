b92284f412c0a7a6614e134c0b7b36b4bde0fdda
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-03 18:31:48 +0530

    selection from ownProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component{
+  componentDidMount(){
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    //posts[this.props.params.id]; // the post we want to show
+    this.props === ownProps;
     return(
       <div>
         Posts Show!
@@ -10,4 +19,9 @@ class PostsShow extends Component{
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps){
+  //return { posts };
+  return { post: post[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost }) (PostsShow);
