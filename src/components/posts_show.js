1986809baecbf0b953429abd8304a93165b6df3b
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-03 19:36:41 +0530

    Data dependencies

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
 
-class PostsShow extends Component{
-  componentDidMount(){
+class PostsShow extends Component {
+  componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    this.props.fetchPost(id)
   }
 
   render() {
-    //posts[this.props.params.id]; // the post we want to show
-    this.props === ownProps;
-    return(
+    const {post} = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
+    return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
-    );
+    )
   }
 }
-
-function mapStateToProps({ posts }, ownProps){
-  //return { posts };
-  return { post: post[ownProps.match.params.id] };
+function mapStateToProps({posts}, ownProps){
+  return {post: posts[ownProps.match.params.id]}
 }
-
-export default connect(mapStateToProps, { fetchPost }) (PostsShow);
+export default connect(mapStateToProps, {fetchPost})(PostsShow)
